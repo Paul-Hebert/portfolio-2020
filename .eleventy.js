426528f6f4f9e35003a54257b6a1d418a7102fe6
Paul Hebert <paul@cloudfour.com>
2020-03-21 14:34:14 -0700

    Fix SCSS hot reloading

--- .eleventy.js
+++ .eleventy.js
@@ -22,7 +22,7 @@ module.exports = eleventyConfig => {
   // Rebuild the site when CSS or JS is updated
   // By default 11ty wouldn't know to watch these files since they're processed
   // by node-sass and rollup.
-  eleventyConfig.addWatchTarget("./src/assets/");
+  eleventyConfig.addWatchTarget("./src/**/*.{js,scss}");
 
   eleventyConfig.addCollection("writing", function(collection) {
     return collection.getFilteredByGlob("src/writing/*/*.md");
