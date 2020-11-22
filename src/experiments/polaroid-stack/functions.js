f9952d10a452c1f568d86945e0971ce3216772f3
Paul Hebert <paul@cloudfour.com>
2020-11-22 13:07:25 -0800

    Fine tune polaroid development timing

--- src/experiments/polaroid-stack/functions.js
+++ src/experiments/polaroid-stack/functions.js
@@ -2,13 +2,13 @@ const polaroids = document.querySelectorAll('.polaroid');
 
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
-    if (entry.intersectionRatio > 0) {
+    if (entry.intersectionRatio === 1) {
       entry.target.classList.remove('polaroid--undeveloped');
       entry.target.classList.add('polaroid--is-developing');
     }
   });
 }, {
-  threshold: 0.9
+  threshold: 1
 });
 
 polaroids.forEach(polaroid => {
